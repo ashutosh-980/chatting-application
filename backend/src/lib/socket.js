6d0e8df5598082ea9ff92d75c48d109398bffb19
unknown <ashutoshmishra110803@gmail.com>
2025-11-30 17:24:59 +0530

    cors again

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -7,7 +7,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["https://chatting-application-black.vercel.app/"],
+    origin: ["https://chatting-application-black.vercel.app"],
   },
 });
 
